1bcace7abfc10ed63b60be09b220a97de01a495b
oprbguitar <oprbguitar@gmail.com>
2013-05-27 20:22:49 -0500

    first comment

---
+++ app/models/tweet.rb
@@ -0,0 +1,8 @@
+class Tweet < ActiveRecord::Base
+Twitter.configure do |config|
+  config.consumer_key = 49qJyhmQpEtgtlYiAToA
+  config.consumer_secret = IidF8gpiJP0W7Vennb68oy3RvVBeNWHSgdzNPsHWk
+  config.oauth_token = 51323073-SftK3FzFCoU8hpCGvBZSZ1tFfBXuYT8sGdxnfJD28
+  config.oauth_token_secret = u7dRa46KmV74V3i7Y5p4hz0CjrypVaGlKpVCr5xq31g
+end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TweeterApp::Application.config.secret_token = 'f5882047066e6c66bca96092aea53dff18578019945441ade92a207037ec74a229b7980ef1b4145f21e92363ab259103a5eaa53bf1db3ef80dd3a22a69ea7279'
